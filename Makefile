45fab7c4a2d7057bbef2ef6b418e0a470d70eec0
Alex G <isaid.zx@gmail.com>
2020-08-27 18:13:30 +0000

    test fix

--- Makefile
+++ Makefile
@@ -3,9 +3,8 @@ install:
 		pip install -r requirements.txt
 
 test:
-	#python -m pytest test_gcli.py
-	python -m pytest -vv --cov=gcli test_gcli.py
-	#python -m pytest --nbval notebook.ipynb
+	python -m pytest -vv --cov=mylib --cov=hello test_hello.py
+
 
 
 lint:
--- test_hello.py
+++ test_hello.py
@@ -1,1 +1,11 @@
-"""this is a test"""
+from click.testing import CliRunner
+from hello import calculate
+
+
+#search(path, ftype):
+    
+def test_calculate():
+  runner = CliRunner()
+  result = runner.invoke(calculate, ['--value1', '1', '--value2', '2'])
+  assert result.exit_code == 0
+  assert '3' in result.output
